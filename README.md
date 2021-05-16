31cd076f4f3d4b0f82d32ce44d13924404a12b36
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-05-16 15:49:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# basic-go-kit
+
+This repository is based on the [Basic Microservices with the Gokit Toolkit](https://www.youtube.com/watch?v=sjd2ePF3CuQ&t=388s) from Youtube.
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	Name      string    `json:"name"`
+	IsActive  bool      `json:"isActive"`
+	Timestamp time.Time `json:"timeStamp"`
+}
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/home", helloWorld).Methods("GET")
+
+	http.ListenAndServe(":8000", router)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Name:      "Write presentation",
+		IsActive:  true,
+		Timestamp: time.Now(),
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(user)
+}
